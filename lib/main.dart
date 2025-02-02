cd1333af0b5478b3cc569254c11cbe6ca0f6b547
Tech-Voyager1 <adimp356@gmail.com>
2025-02-02 15:24:34 +0530

    firebase_configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:dis_manag/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'mapScreen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(Home());
 }
 
